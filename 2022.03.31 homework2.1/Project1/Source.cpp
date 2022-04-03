c65332c8ff083e3a0c3aed6bc1295ae0e986376d
Ася Шайхутдинова <shaykhytdiniva@mail.ru>
2022-04-03 23:59:00 +0300

    2022.04.03 homework2.1

---
+++ 2022.03.31 homework2.1/Project1/Source.cpp
@@ -0,0 +1,15 @@
+#include "Complex.h"
+
+int main()
+{
+	Complex z1(3, 1);
+	Complex z2(5, -2);
+	cout << z1 * z2 << endl;
+	cout << z1 / z2 << endl;
+	cout << 3 * z1 + 4 * z2 << endl;
+	cout << z1.abs() << endl;
+	cout << z2.arg() << endl;
+	cout << z1.reverse() << endl;
+	z1.roots(5, cout);
+	return 0;
+}
